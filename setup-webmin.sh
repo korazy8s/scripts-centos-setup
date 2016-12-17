092746c23b2fafadb53e76afbe67aecd21cb7024
korazy8s <korazy@gmail.com>
2016-12-17 13:24:11 -0600

    Create setup-webmin.sh

---
+++ setup-webmin.sh
@@ -0,0 +1,26 @@
+#/bin/sh
+
+# add webmin to yum repos
+cat > /etc/yum.repos.d/webmin.repo <<EOL
+[Webmin]
+name=Webmin Distribution Neutral
+#baseurl=http://download.webmin.com/download/yum
+mirrorlist=http://download.webmin.com/download/yum/mirrorlist
+enabled=1
+EOL
+
+# add webmin key to yum
+rpm --import http://www.webmin.com/jcameron-key.asc
+
+# update yum
+yum check-update
+
+# install
+yum -y install webmin
+chkconfig webmin on
+service webmin start
+service webmin enable
+
+# open firewall
+firewall-cmd --add-port=10000/tcp --permanent
+systemctl restart firewalld
