a070499ce7b943223df0ddf5872a6a882785c799
korazy8s <korazy@gmail.com>
2016-12-17 20:20:52 -0600

    Create showinfo.sh

---
+++ showinfo.sh
@@ -0,0 +1,24 @@
+$!/bin/sh
+
+# show host information
+hostnamectl
+
+#show network information
+ip addr show
+
+# show ip ports
+netstat -tulpn
+
+# show firewall status
+firewall-cmd --state
+firewall-cmd --list-all
+
+# show apache status
+systemctl status httpd
+
+# show php information
+php --version
+phpunit --version
+
+# show selinux status
+sestatus
