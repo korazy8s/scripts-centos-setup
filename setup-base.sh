3a74ef9464f4a4d53b1e1347b5e5fc2241244b46
korazy8s <korazy@gmail.com>
2017-04-01 16:47:12 -0500

    Create setup-base.sh

---
+++ setup-base.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# set variables
+HOSTNAME=www1
+CERTC=CA
+CERTST=Manitoba
+CERTO=
+CERTLOCALITY=
+CERTCOMMONNAME=
+CERTORG=
+CERTEMAIL=root@localhost
+
+# add webmin to yum repos
+cat > /etc/yum.repos.d/webmin.repo <<EOL
+[Webmin]
+name=Webmin Distribution Neutral
+#baseurl=http://download.webmin.com/download/yum
+mirrorlist=http://download.webmin.com/download/yum/mirrorlist
+enabled=1
+EOL
+
+# add webmin key to yum
+rpm --import http://www.webmin.com/jcameron-key.asc
+
+# update yum
+yum check-update
+
+# install latest updates
+yum -y update
+
+# setup automatic updates manager
+yum -y install yum-cron
+systemctl start yum-cron
+sed -ie 's/apply_updates = no/apply_updates = yes/' /etc/yum/yum-cron.conf
+
+# set machine info
+hostnamectl set-hostname ${HOSTNAME}
+
+# install dependencies
+yum -y install wget
+yum -y install firewalld
+
+# install vmware tools
+yum -y install open-vm-tools
+
+# start and setup firewall
+systemctl restart firewalld
+firewall-cmd --zone=public --add-port=ssh/tcp --permanent
+firewall-cmd --add-port=10000/tcp --permanent
+systemctl restart firewalld
+
+# install
+yum -y install webmin
+chkconfig webmin on
+service webmin start
