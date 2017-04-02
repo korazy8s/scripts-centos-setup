691e008fe0584384951426b2e3a412a3f80d5333
korazy8s <korazy@gmail.com>
2017-04-02 15:44:23 -0500

    Update showinfo.sh

--- showinfo.sh
+++ showinfo.sh
@@ -5,23 +5,30 @@ hostnamectl
 lsb_release -a
 uname -a
 dpkg -l | grep linux-image
+echo
 
 #show network information
 ip addr show
+echo
 
 # show ip ports
 netstat -tulpn
+echo
 
 # show firewall status
 firewall-cmd --state
 firewall-cmd --list-all
+echo
 
 # show apache status
 systemctl status httpd
+echo
 
 # show php information
 php --version
 phpunit --version
+echo
 
 # show selinux status
 sestatus
+echo
