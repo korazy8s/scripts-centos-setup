ad9a07cb12282369c97db9567165ab7b502aee4c
korazy8s <korazy@gmail.com>
2017-08-31 10:58:51 -0500

    Update install-git-client.sh

--- install-git-client.sh
+++ install-git-client.sh
@@ -3,3 +3,4 @@ sudo yum -y install git
 git --version
 git clone https://github.com/korazy8s/scripts-centos-setup .
 git fetch origin && git reset --hard origin/master && chmod +x *.sh
+chmod +x *.sh
